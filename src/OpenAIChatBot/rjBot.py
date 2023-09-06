94d3a4f22899873475ce33e6646530719adf4df5
Python Port <rajeshjhajnv@gmail.com>
2023-09-06 08:48:20 +0530

    Rajesh :+1: Open AI chatbot just like chatgpt implementation along with gradio server.
    Rajesh :+1: Open AI chatbot just like chatgpt implementation along with gradio server.

---
+++ src/OpenAIChatBot/rjBot.py
@@ -0,0 +1,26 @@
+'''
+Created on Aug 26, 2023
+
+@author: Admin
+'''
+import requests
+
+URL = "https://api.openai.com/v1/chat/completions"
+
+payload = {
+"model": "gpt-3.5-turbo",
+"messages": [{"role": "user", "content": f"What is the first computer in the world?"}],
+"temperature" : 1.0,
+"top_p":1.0,
+"n" : 1,
+"stream": False,
+"presence_penalty":0,
+"frequency_penalty":0,
+}
+
+headers = {
+"Content-Type": "application/json",
+"Authorization": f"Bearer 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"
+}
+
+response = requests.post(URL, headers=headers, json=payload, stream=False)
